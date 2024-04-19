adb9983b06839775547ebea812a4a34ed747976c
KaltrinaKrasniqi <116883212+KaltrinaKrasniqi@users.noreply.github.com>
2024-04-20 00:34:56 +0200

    Update login.php

--- login-system-with-email-verification/endpoint/login.php
+++ login-system-with-email-verification/endpoint/login.php
@@ -8,3 +8,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt = $conn->prepare("SELECT `password` FROM `tbl_user` WHERE `username` = :username");
     $stmt->bindParam(':username', $username);
     $stmt->execute();
+    
+    if ($stmt->rowCount() > 0) {
+        $row = $stmt->fetch();
+        $stored_password = $row['password'];
+
+        if ($password === $stored_password) {
+            echo "
+            <script>
+                alert('Login Successfully!');
+                window.location.href = 'http://localhost/Tech_Shop_Website_Gr.6-main/';
+            </script>
+            "; 
+        } else {
+            echo "
+            <script>
+                alert('Login Failed, Incorrect Password!');
+                window.location.href = 'http://localhost/login-system-with-email-verification/login.php';
+            </script>
+            ";
+        }
+    } else {
+        echo "
+            <script>
+                alert('Login Failed, User Not Found!');
+                window.location.href = 'http://localhost/login-system-with-email-verification/login.php';
+            </script>
+            ";
+    }
+}
