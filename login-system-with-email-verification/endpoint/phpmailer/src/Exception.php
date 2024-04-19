03e55a1cd52443626c2822ed960f0ae8d8d9d044
KaltrinaKrasniqi <116883212+KaltrinaKrasniqi@users.noreply.github.com>
2024-04-20 00:21:35 +0200

    Update Exception

--- login-system-with-email-verification/endpoint/phpmailer/src/Exception.php
+++ login-system-with-email-verification/endpoint/phpmailer/src/Exception.php
@@ -17,3 +17,23 @@
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.
  */
+
+namespace PHPMailer\PHPMailer;
+
+/**
+ * PHPMailer exception handler.
+ *
+ * @author Marcus Bointon <phpmailer@synchromedia.co.uk>
+ */
+class Exception extends \Exception
+{
+    /**
+     * Prettify error message output.
+     *
+     * @return string
+     */
+    public function errorMessage()
+    {
+        return '<strong>' . htmlspecialchars($this->getMessage(), ENT_COMPAT | ENT_HTML401) . "</strong><br />\n";
+    }
+}
