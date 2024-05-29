628753a36b4227130250f4c4294ca3b7ddf5b128
ereza4 <erezatemaj@gmail.com>
2024-05-29 15:50:02 +0200

    index update

--- index.php
+++ index.php
@@ -317,68 +317,6 @@ $currentUser = new User($user_id,$username,$password,$first_name,$last_name,$con
         </div>
       </div>
     </section>
-    <section id="latest-blog" class="padding-large">
-      <div class="container">
-        <div class="row">
-          <div class="display-header d-flex justify-content-between pb-3">
-            <h2 class="display-7 text-dark text-uppercase">Latest Posts</h2>
-            <div class="btn-right">
-             
-            </div>
-          </div>
-          <div class="post-grid d-flex flex-wrap justify-content-between">
-            <div class="col-lg-4 col-sm-12">
-              <div class="card border-none me-3">
-                <div class="card-image">
-                  <img src="images/post-item1.jpg" alt="" class="img-fluid">
-                </div>
-              </div>
-              <div class="card-body text-uppercase">
-                <div class="card-meta text-muted">
-                  <span class="meta-date">feb 22, 2023</span>
-                  <span class="meta-category">- Gadgets</span>
-                </div>
-                <h3 class="card-title">
-                  <a href="#">Get some cool gadgets in 2023</a>
-                </h3>
-              </div>
-            </div>
-            <div class="col-lg-4 col-sm-12">
-              <div class="card border-none me-3">
-                <div class="card-image">
-                  <img src="images/post-item2.jpg" alt="" class="img-fluid">
-                </div>
-              </div>
-              <div class="card-body text-uppercase">
-                <div class="card-meta text-muted">
-                  <span class="meta-date">feb 25, 2023</span>
-                  <span class="meta-category">- Technology</span>
-                </div>
-                <h3 class="card-title">
-                  <a href="#">Technology Hack You Won't Get</a>
-                </h3>
-              </div>
-            </div>
-            <div class="col-lg-4 col-sm-12">
-              <div class="card border-none me-3">
-                <div class="card-image">
-                  <img src="images/post-item3.jpg" alt="" class="img-fluid">
-                </div>
-              </div>
-              <div class="card-body text-uppercase">
-                <div class="card-meta text-muted">
-                  <span class="meta-date">feb 22, 2023</span>
-                  <span class="meta-category">- Camera</span>
-                </div>
-                <h3 class="card-title">
-                  <a href="#">Top 10 Small Camera In The World</a>
-                </h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
     <section id="testimonials" class="position-relative">
       <div class="container">
         <div class="row">
